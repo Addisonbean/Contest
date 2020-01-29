9cef71638dee0767840f8e1b3556367aea143a28
Addison Bean <addisonbean@gmail.com>
2020-01-29 13:13:52 -0600

    Add a home page

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h2>Welome!!!</h2>
+
+		<p>idk to put here</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+};
+</script>
+
+<style scoped>
+h2 {
+	text-align: center;
+}
+</style>
