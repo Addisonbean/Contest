81633729786ebb4b1db2d40274d666ad316999f4
Addison Bean <addisonbean@gmail.com>
2020-01-29 15:41:03 -0600

    Use vue-router for navigation

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+import Home from './pages/Home.vue';
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+	mode: 'history',
+	routes: [
+		{ path: '/', component: Home },
+	],
+});
 
 new Vue({
 	render: h => h(App),
-}).$mount('#app')
+	router,
+}).$mount('#app');
