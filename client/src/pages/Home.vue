d088601a255cad9461f83a039f17336db0f1a0ee
Jhisscock <jthisscock@mail.lipscomb.edu>
2020-02-18 19:29:15 -0600

    documentation done

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,19 +1,24 @@
 <template>
 	<div>
-	<p>
-	Test
-	</p>
+
+		<ul>
+			<li><a href ="/docs/javaDoc/index.html" target = "_blank">Java Documentation</a></li>
+			<li><a href ="/docs/pythonDoc/index.html" target = "_blank">Python Documentation</a></li>
+		</ul>
 	</div>
 </template>
 
 <script>
-export default {
-	name: 'Home',
-};
+	export default {
+		name: 'Home',
+	};
 </script>
 
 <style scoped>
-h2 {
-	text-align: center;
-}
+	h2 {
+		text-align: center;
+	}
+	ul{
+		text-align: left;
+	}
 </style>
