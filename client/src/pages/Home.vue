b52d03a7a83e46bcb89a15e83839bbf51d88dd81
Jhisscock <jthisscock@mail.lipscomb.edu>
2020-02-25 17:42:09 -0600

    Update Home.vue

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,13 +1,9 @@
 <template>
 	<div>
-<<<<<<< HEAD
-=======
-
 		<ul>
 			<li><a href ="/docs/javaDoc/index.html" target = "_blank">Java Documentation</a></li>
 			<li><a href ="/docs/pythonDoc/index.html" target = "_blank">Python Documentation</a></li>
 		</ul>
->>>>>>> 2ebf7c081c9c66a7d1ba39f1a0b19980d95ee2af
 	</div>
 </template>
 
