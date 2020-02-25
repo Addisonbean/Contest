36ee18e5964d00ac7020f10d1db9a011216b762d
Jhisscock <jthisscock@mail.lipscomb.edu>
2020-02-25 17:25:09 -0600

    problem routing started, need to connect to db

---
+++ client/src/pages/Problems.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+        <router-link :to="{ name: 'ProblemViewer', params: { problem: this.problemName }}">{{problemName}}</router-link>
+	</div>
+</template>
+
+<script>
+	export default {
+        data(){
+            return{
+                problemName: "Hello_World"
+            }
+        },
+        name: 'Problems',
+	};
+</script>
+
+<style scoped>
+	h2 {
+		text-align: center;
+	}
+	ul{
+		text-align: left;
+	}
+</style>
