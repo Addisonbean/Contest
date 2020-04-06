0745d0fd48b9f32a3b9ac09c4693538a9b7ca8ed
zachariahwestover <35404759+zachariahwestover@users.noreply.github.com>
2020-04-06 00:54:14 -0400

    Updated Problem Viewer

--- client/src/pages/ProblemViewer.vue
+++ client/src/pages/ProblemViewer.vue
@@ -1,17 +1,29 @@
 <template>
 	<div v-if="Object.keys(problem).length">
-		<div>
-			<h1>{{problem.title}}</h1>
+		<div class="container">
+			<div>
+				<h1>{{problem.title}}</h1>
+			</div>
 			<p>{{problem.backgroundInfo}}</p>
 			<p>{{problem.description}}</p>
-			<h1>Input Description</h1>
+			<h2>Input Description</h2>
 			<p>{{problem.inputDescription}}</p>
-			<h1>Output Description</h1>
+			<h2>Output Description</h2>
 			<p>{{problem.outputDescription}}</p>
-			<h1>Example Input</h1>
-			<p>{{problem.exampleInput}}</p>
-			<h1>Example Output</h1>
-			<p>{{problem.exampleOutput}}</p>
+			<div class="wrapper clearfix">
+				<div class="input">
+					<h3>Example Input</h3>
+					<div class="console">
+						<p>{{problem.exampleInput}}</p>
+					</div>
+				</div>
+				<div class="output">
+					<h3>Example Output</h3>
+					<div class="console">
+						<p>{{problem.exampleOutput}}</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div v-else>
@@ -47,4 +59,37 @@ export default {
 	ul{
 		text-align: left;
 	}
+	p {
+		text-align: left;
+	}
+	.container {
+        width: 100%;
+        background: #f2f2f2; 
+    }
+	.input, .output {
+        float: left; 
+        min-height: 250px;
+        box-sizing: border-box;
+    }
+    .input {            
+        width: 50%;
+    }
+    .output {
+        width: 50%;
+    }
+	.console {
+		font-family: "Lucida Console", Courier, monospace;
+		font-size: 14px;
+		padding: 10px;
+        background: #d4d7dc;
+		min-height: 195px;
+		border: solid 1px;
+	}
+	.clearfix:after {
+        content: ".";
+        display: block;
+        height: 0;
+        clear: both;
+        visibility: hidden;
+    }
 </style>
